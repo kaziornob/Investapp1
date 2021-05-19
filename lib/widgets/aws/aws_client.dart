a3e7e375a38bebd2844e28a696e71132961ab8ac
Sintoo <abhinavkumarsintoo@gmail.com>
2021-05-19 22:55:37 +0530

    aws client

--- lib/widgets/aws/aws_client.dart
+++ lib/widgets/aws/aws_client.dart
@@ -1,10 +1,14 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+import 'package:path/path.dart' as fileUtil;
 import 'dart:typed_data';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:amazon_cognito_identity_dart/sig_v4.dart';
 import 'package:path_provider/path_provider.dart';
+
+typedef void OnUploadProgressCallback(int sentBytes, int totalBytes);
 
 class AWSClient {
   String accessKeyId =
@@ -17,14 +21,18 @@ class AWSClient {
   String bucketname = "auro-invest"; // replace with your S3's bucket name
   String s3Endpoint =
       'https://auro-invest.s3-us-west-2.amazonaws.com/'; // update the endpoint url for your bucket
-  static var httpClient = new HttpClient();
+  static var httpClient = HttpClient();
 
-  Future<String> uploadData(String folderName, String fileName, Uint8List data) async {
+  http.StreamedResponse response;
+
+  Future<String> uploadData(
+      String folderName, String fileName, Uint8List data) async {
     final length = data.length;
     print(length);
     final Uri uri = Uri.parse(s3Endpoint);
     print(uri.path);
-    final req = http.MultipartRequest("POST", uri);
+
+    final http.MultipartRequest req = http.MultipartRequest("POST", uri);
     final multipartFile = http.MultipartFile(
       'file',
       http.ByteStream.fromBytes(data),
@@ -50,10 +58,15 @@ class AWSClient {
 
     try {
       print("sending file");
-      final http.StreamedResponse res = await req.send();
-      print(res.statusCode);
-      print(res.headers);
-      return res.headers["location"];
+
+      response = await req.send();
+
+      response.stream.listen((value) {
+        print("${value.toString()}");
+      });
+      print(response.statusCode);
+      print(response.headers);
+      return response.headers["location"];
 
       // HOW RESPONSE HEADERS FORMAT LOOKS LIKE
       // {etag: "8aecd69c16644b3cb6317e7b6aa358a7", x-amz-request-id: 5JH6J8EJTZZKP9YP,
@@ -61,7 +74,6 @@ class AWSClient {
       // date: Thu, 13 May 2021 08:26:42 GMT,
       // x-amz-id-2: Wg43oNy89vrrjnEsgFNnhPNcs/DGdg+SO7YuzSfzylDgFhC9iKawTiIxliKpseB2p6KEVz1A/Fw=,
       // server: AmazonS3}
-
 
       // await for (var value in res.stream.transform(utf8.decoder)) {
       //   print(value);
@@ -73,6 +85,107 @@ class AWSClient {
 
       return "";
     }
+  }
+
+  fileUploadMultipart({
+    File file,
+    OnUploadProgressCallback onUploadProgress,
+    String folderName,
+    String fileName,
+  }) async {
+    assert(file != null);
+
+    final url = '$s3Endpoint';
+    final Uri uri = Uri.parse(s3Endpoint);
+    // final httpClient = getHttpClient();
+
+    final request = await httpClient.postUrl(Uri.parse(url));
+
+    int byteCount = 0;
+
+    var multipart = await http.MultipartFile.fromPath(
+        fileUtil.basename(file.path), file.path);
+
+    // final fileStreamFile = file.openRead();
+
+    // var multipart = MultipartFile("file", fileStreamFile, file.lengthSync(),
+    //     filename: fileUtil.basename(file.path));
+
+    var requestMultipart = http.MultipartRequest("PUT", uri);
+
+    final policy = Policy.fromS3PresignedPost(folderName + '/' + fileName,
+        bucketname, accessKeyId, 15, requestMultipart.contentLength,
+        region: region);
+    final key =
+        SigV4.calculateSigningKey(secretKeyId, policy.datetime, region, 's3');
+    final signature = SigV4.calculateSignature(key, policy.encode());
+
+    requestMultipart.files.add(multipart);
+    requestMultipart.fields['key'] = policy.key;
+    requestMultipart.fields['acl'] = 'public-read';
+    requestMultipart.fields['X-Amz-Credential'] = policy.credential;
+    requestMultipart.fields['X-Amz-Algorithm'] = 'AWS4-HMAC-SHA256';
+    requestMultipart.fields['X-Amz-Date'] = policy.datetime;
+    requestMultipart.fields['Policy'] = policy.encode();
+    requestMultipart.fields['X-Amz-Signature'] = signature;
+
+    var msStream = requestMultipart.finalize();
+
+    var totalByteLength = requestMultipart.contentLength;
+    print(totalByteLength);
+
+    request.contentLength = totalByteLength;
+
+    request.headers.set(HttpHeaders.contentTypeHeader,
+        requestMultipart.headers[HttpHeaders.contentTypeHeader]);
+
+    Stream<List<int>> streamUpload = msStream.transform(
+      StreamTransformer.fromHandlers(
+        handleData: (data, sink) {
+          sink.add(data);
+
+          byteCount += data.length;
+          print(byteCount);
+
+          if (onUploadProgress != null) {
+            onUploadProgress(byteCount, totalByteLength);
+            // CALL STATUS CALLBACK;
+          }
+        },
+        handleError: (error, stack, sink) {
+          print("stream error ");
+          throw error;
+        },
+        handleDone: (sink) {
+          print("stream done");
+          sink.close();
+          // UPLOAD DONE;
+        },
+      ),
+    );
+
+    await request
+        .addStream(streamUpload)
+        .whenComplete(() => print("completed"))
+        .catchError((err) {
+          print(err.toString());
+          print("error on upload");
+    });
+
+    final httpResponse = await request.close();
+//
+    var statusCode = httpResponse.statusCode;
+    print(statusCode);
+    print(httpResponse.headers);
+    print(httpResponse.reasonPhrase);
+    // print(httpResponse.);
+
+    // if (statusCode ~/ 100 != 2) {
+    //   throw Exception(
+    //       'Error uploading file, Status code: ${httpResponse.statusCode}');
+    // } else {
+    //   return await readResponseAsString(httpResponse);
+    // }
   }
 
   Future<File> downloadFile(String url1, String filename) async {
